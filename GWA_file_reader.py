ee1c2e418140f6c4ceeb750d3dc4507eeed3880d
unknown <arixxdelera21@gmail.com>
2023-04-23 21:40:44 +0800

    text file has been created; initial list for student and their gwa done

--- GWA_file_reader.py
+++ GWA_file_reader.py
@@ -4,6 +4,10 @@
 # The program will outputs the name of the student who got the highest GWA (including the GWA).
 
 # create a student_name&gwa_record.txt and open it
+with open('student_name&gwa_record.txt', 'r') as record_file:
+    for line in record_file:
+        print(line.strip())
+
 # create the lists of students with their gwa to the text file
 # set variables for highest gwa and person with highest gwa
 # use for loop to iterate the file
