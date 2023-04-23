1feada7e27da82e76a12c1484e9185dcc30fbc0c
unknown <arixxdelera21@gmail.com>
2023-04-23 21:54:31 +0800

    sample student name and gwa has been completed; variables has successfully created

--- GWA_file_reader.py
+++ GWA_file_reader.py
@@ -5,11 +5,9 @@
 
 # create a student_name&gwa_record.txt and open it
 with open('student_name&gwa_record.txt', 'r') as record_file:
-    for line in record_file:
-        print(line.strip())
-
-# create the lists of students with their gwa to the text file
-# set variables for highest gwa and person with highest gwa
+    # set variables for highest gwa and person with highest gwa
+    highest_student_name = ''
+    highest_gwa = 5.0
 # use for loop to iterate the file
 # split the name of the student and their gwa
 # convert the gwa into float
