29273c5077b26f330976c4015cfbf67a35c725d5
unknown <arixxdelera21@gmail.com>
2023-04-23 21:28:20 +0800

    Pseudocode done

---
+++ GWA_file_reader.py
@@ -0,0 +1,13 @@
+# Delera, Aritz B.
+
+# Write a Python program that read a file containing the name of 20 students together with their GWA. 
+# The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# create a student_name&gwa_record.txt and open it
+# create the lists of students with their gwa to the text file
+# set variables for highest gwa and person with highest gwa
+# use for loop to iterate the file
+# split the name of the student and their gwa
+# convert the gwa into float
+# create condition to update the file
+# output the result
