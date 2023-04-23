8bc623c9d9b0b46ae7eb9ba0559345f7db309792
unknown <arixxdelera21@gmail.com>
2023-04-23 23:39:40 +0800

    add some imports and prompts

--- GWA_file_reader.py
+++ GWA_file_reader.py
@@ -1,7 +1,22 @@
 # Delera, Aritz B.
 
-# Write a Python program that read a file containing the name of 20 students together with their GWA. 
-# The program will outputs the name of the student who got the highest GWA (including the GWA).
+# Import modules
+import pyfiglet
+import time
+import random
+
+opening = pyfiglet.figlet_format("= O.O.P =", font="starwars")
+print(opening)
+
+# Create an introduction
+print("=" * 61)
+print(" Welcome to AritzMetic's GWA Reader! ".center(60, "+"))
+print("=" * 61)
+
+# Ask the name of the user to create a greeting
+name = input("\033[30mHi Smart Pipol! what is your name?: \033[0m")
+print()
+print("\033[40mHi", name, "! AritzMetic is here to help you in reading the Highest GWA!!\033[0m")
 
 # create a student_name&gwa_record.txt and open it
 with open('student_name&gwa_record.txt', 'r') as record_file:
@@ -20,4 +35,19 @@ with open('student_name&gwa_record.txt', 'r') as record_file:
             highest_gwa = gwa
             
 # output the result
-print(f'Congratulations to {highest_student_name} for having the highest GWA of {highest_gwa}!')
+    print()
+    print("Calculating results...")
+    time.sleep(3)
+    print()
+    print(f'Congratulations to {highest_student_name} for having the highest GWA of {highest_gwa}!')
+    time.sleep(3)
+
+# Before exiting the application, include some pauses and random message prompts.
+print()
+print("loading...")
+time.sleep(3)
+print()
+print(random.choice(["Thank you for using AritzMetic's GWA Reader!", "Goodbye and have a nice day!", "Stay curious and keep learning!"]))
+time.sleep(2)
+closing = pyfiglet.figlet_format(" = THE END = ", font="doom")
+print(closing)
